bbb3be84d526bddb89186877f6479dc3d2411121
severogianluca <gianlucasevero300@gmail.com>
2025-04-09 16:30:30 +0200

    Feat: create alcol list

---
+++ src/data/alcolici.js
@@ -0,0 +1,30 @@
+const alcolici = [
+
+    {
+        id: 1,
+        etichetta: "Bulleit",
+        category: "Bourbon Wisky"
+    },
+    {
+        id: 2,
+        etichetta: "Expolon",
+        category: "Tequila"
+    },
+    {
+        id: 3,
+        etichetta: "Poli",
+        category: "Grappa Barrique"
+    },
+    {
+        id: 4,
+        etichetta: "N.3",
+        category: "Gin"
+    },
+    {
+        id: 5,
+        etichetta: "Grey Goose",
+        category: "Vodka"
+    }
+]
+
+export default alcolici
