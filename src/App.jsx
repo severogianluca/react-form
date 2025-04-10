fae8c89d31b18f8f7d663fb907019f2701af64d4
severogianluca <gianlucasevero300@gmail.com>
2025-04-10 09:57:53 +0200

    codice formattato

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,70 @@
 import alcolici from './data/alcolici';
-import { useState } from 'react'
-function App() {
+import { useState } from 'react';
 
-  const [article, setArticle] = useState(alcolici)
+function App() {
+  const [article, setArticle] = useState(alcolici);
   const [newArticles, setNewArticle] = useState('');
 
-  const addArticle = event =>{
+  const addArticle = (event) => {
     event.preventDefault();
-    const newAlcolic = ([...article, newArticles]);
+    const newAlcolic = [...article, newArticles];
     setArticle(newAlcolic);
     console.log(newAlcolic);
-    setNewArticle("");
-  }
+    setNewArticle('');
+  };
 
   const removeAlcolic = (deleteAlcolic) => {
-    const newArticles = article.filter((element, index) => {
-      return deleteAlcolic !== index
-
-    })
+    const newArticles = article.filter((_, index) => deleteAlcolic !== index);
     setArticle(newArticles);
-  }
-
+  };
 
   return (
     <>
-    <div className='container'>
-    <div>
-        <h1>Lista alcolici</h1>
-    </div>
-    <div className='container-list'>
-    <ul class="list-group">
-        {article.map((element,index) => (
-          <li  class="list-group-item d-flex justify-content-between align-items-center" key={index}>
-            {element}
+      <div className="container">
+        <div>
+          <h1>Lista alcolici</h1>
+        </div>
 
-          <button 
-          onClick={() => removeAlcolic(index)}
-          type="button" class="btn btn-danger">Elimina
-          </button>
-          </li>
+        <div className="container-list">
+          <ul className="list-group">
+            {article.map((element, index) => (
+              <li
+                className="list-group-item d-flex justify-content-between align-items-center"
+                key={index}
+              >
+                {element}
+                <button
+                  onClick={() => removeAlcolic(index)}
+                  type="button"
+                  className="btn btn-danger"
+                >
+                  Elimina
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-        ))}
-    </ul> 
-
-
-
-    </div>
-    <form onSubmit={addArticle}>
-      <div className="d-flex justify-content-between align-items-center mt-5 gap-3">
-    <div class="form-floating mt-5 dimensione">
-    <input 
-        class="form-control"
-        id='list'
-        type="text"
-        value={newArticles}
-        onChange={event => { setNewArticle(event.target.value) }}
-    />
-     <label for="list">Aggiungi al carrello </label>
-    </div>
-    <button  type="button" class="btn btn-primary mt-3">Invia</button>
-    </div>
-    </form>
-    </div>
-    
+        <form onSubmit={addArticle}>
+          <div className="d-flex justify-content-between align-items-center mt-5 gap-3">
+            <div className="form-floating mt-5 dimensione">
+              <input
+                className="form-control"
+                id="list"
+                type="text"
+                value={newArticles}
+                onChange={(event) => setNewArticle(event.target.value)}
+              />
+              <label htmlFor="list">Aggiungi al carrello</label>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3">
+              Invia
+            </button>
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
